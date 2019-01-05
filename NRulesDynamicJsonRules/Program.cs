eea4ed0f1ce509e2259722a52b7e41f9b0b3cc62
lincoln42 <lincoln42@hotmail.com>
2019-01-05 13:59:27 +0000

    Initial Version

---
+++ NRulesDynamicJsonRules/Program.cs
@@ -0,0 +1,98 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using NRules;
+using NRules.Fluent;
+using NRules.Fluent.Dsl;
+using NRules.RuleModel;
+using RuleModels;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NRulesDynamicJsonRules
+{
+    class Program
+    {
+     
+
+        static void Main(string[] args)
+        {
+            RunJsonRuleClassesFromGeneratedDll();
+            Console.WriteLine("Done");
+            Console.ReadLine();
+        }
+
+        static void RunJsonRuleClassesFromGeneratedDll()
+        {
+            var ruleRepo = new RuleRepository();
+            var loc = Assembly.GetExecutingAssembly().Location;
+            var assemblyFileInfo = new FileInfo(Assembly.GetExecutingAssembly().Location);
+            var rulesAssemblyPath = Path.Combine(assemblyFileInfo.Directory.FullName, @"Rules\JsonRulesAssembly.dll");
+            var rulesAssemblyFileInfo = new FileInfo(rulesAssemblyPath);
+
+            var rulesClassLoc = @"Rules\JsonRules.cs";
+
+            if (!rulesAssemblyFileInfo.Exists)
+            {
+                var source = string.Empty;
+                using (var sr = new StreamReader(rulesClassLoc))
+                {
+                    source = sr.ReadToEnd();
+                }
+
+                var syntaxTree = CSharpSyntaxTree.ParseText(source);
+                var mscorlib = MetadataReference.CreateFromFile(typeof(object).Assembly.Location);
+                var newtonSoft = MetadataReference.CreateFromFile(typeof(JsonWriter).Assembly.Location);
+                var newtonSoftLinq = MetadataReference.CreateFromFile(typeof(JObject).Assembly.Location);
+                var ruleModels = MetadataReference.CreateFromFile(typeof(RuleResult<string[]>).Assembly.Location);
+                var nRule = MetadataReference.CreateFromFile(typeof(IAgenda).Assembly.Location);
+                var nRuleModel = MetadataReference.CreateFromFile(typeof(IContext).Assembly.Location);
+                var nRuleDsl = MetadataReference.CreateFromFile(typeof(Rule).Assembly.Location);
+                var linq = MetadataReference.CreateFromFile(typeof(IQueryable).Assembly.Location);
+
+                var compilation = CSharpCompilation.Create("JsonRulesAssembly", syntaxTrees: new[] { syntaxTree }, references: new[] {
+                mscorlib, newtonSoft, newtonSoftLinq, ruleModels, nRule, nRuleModel, nRuleDsl, linq},
+                options: new CSharpCompilationOptions(outputKind: OutputKind.DynamicallyLinkedLibrary,
+                optimizationLevel: OptimizationLevel.Release));
+
+                var emitResult = compilation.Emit(rulesAssemblyFileInfo.FullName);
+
+                if (!emitResult.Success)
+                {
+                    Array.ForEach(emitResult.Diagnostics.ToArray(), d => Console.WriteLine(d));
+                    return;
+                }
+            }
+
+            var rulesAssembly = Assembly.LoadFile(rulesAssemblyPath);
+            ruleRepo.Load(x => x.From(rulesAssembly));
+
+            var factory = ruleRepo.Compile();
+            var session = factory.CreateSession();
+
+            var jsonFactStr = @"{questions:[{number:1, answer: true}, {number:2, answer:true}]}";
+            var jsonFactObj = JObject.Parse(jsonFactStr);
+
+            session.Insert(jsonFactObj);
+            session.Fire();
+
+            var results = new HashSet<string>();
+
+            foreach(var rr in session.Query<RuleResult<string[]>>())
+            {
+                if ( rr.Result != null)
+                {
+                    Array.ForEach(((string[])rr.Result), x => results.Add(x));
+                }
+            }
+
+            Array.ForEach(results.ToArray(), x => Console.WriteLine(x));
+        }
+    }
+}
---
+++ NRulesDynamicJsonRules/Rules/JsonRules.cs
@@ -0,0 +1,48 @@
+﻿using Newtonsoft.Json.Linq;
+using NRules.Fluent.Dsl;
+using RuleModels;
+using System.Linq;
+
+namespace NRulesDynamicJsonRules.Rules
+{
+    public class OptionA : Rule
+    {
+        public override void Define()
+        {
+            JObject questionSet = null;
+            JObject q = null;
+
+            When()
+                .Match<JObject>(() => questionSet,
+                                qs => qs != null && qs.SelectToken("questions") != null && qs["questions"] is JArray && qs["questions"].HasValues)
+                .Let(() => q,
+                      () => questionSet.SelectToken("questions").FirstOrDefault(x => x.SelectToken("number") != null &&
+                      x["number"] is JValue && x.SelectToken("number").Value<int>() == 1 &&
+                      x.SelectToken("answer") != null && x["answer"] is JValue && x.SelectToken("answer").Value<bool>() == true))
+                .Having(() => q != null);
+            Then()
+                 .Yield(ctx => new RuleResult<string[]> { Result = new string[] { "OptionA" } });
+        }
+    }
+
+    public class OptionB : Rule
+    {
+        public override void Define()
+        {
+            JObject questionSet = null;
+            JObject q = null;
+
+            When()
+                .Match<JObject>(() => questionSet,
+                                qs => qs != null && qs.SelectToken("questions") != null && qs["questions"] is JArray && qs["questions"].HasValues)
+                .Let(() => q,
+                      () => questionSet.SelectToken("questions").FirstOrDefault(x => x.SelectToken("number") != null &&
+                      x["number"] is JValue && x.SelectToken("number").Value<int>() == 2 &&
+                      x.SelectToken("answer") != null && x["answer"] is JValue && x.SelectToken("answer").Value<bool>() == true))
+                .Having(() => q != null);
+            Then()
+                 .Yield(ctx => new RuleResult<string[]> { Result = new string[] { "OptionB" } });
+        }
+    }
+
+}
